a08f59f2111d01e023d362836c7d1ea92f0853fe
HidanYugakure <fidants@code.edu.az>
2024-03-10 15:29:15 +0400

    tapsiriq

--- add.js
+++ add.js
@@ -1,28 +1,28 @@
-class Calculator {
-    constructor(value) {
-        this.value = value;
-    }
+// class Calculator {
+//     constructor(value) {
+//         this.value = value;
+//     }
 
-    plus(num) {
-        this.value += num;
-        return this;
-    }
+//     plus(num) {
+//         this.value += num;
+//         return this;
+//     }
 
-    minus(num) {
-        this.value -= num;
-        return this;
-    }
+//     minus(num) {
+//         this.value -= num;
+//         return this;
+//     }
 
-    multiply(num) {
-        this.value *= num;
-        return this;
-    }
+//     multiply(num) {
+//         this.value *= num;
+//         return this;
+//     }
 
-    divide(num) {
-        this.value /= num;
-        return this;
-    }
-}
+//     divide(num) {
+//         this.value /= num;
+//         return this;
+//     }
+// }
 
 var result = new Calculator(50).plus(6).minus(30).multiply(3).divide(2);
 console.log(result.value);  
@@ -31,12 +31,10 @@ console.log(result.value);
 //2 ci tapsiriq
 const divideButton = document.getElementById("divide");
 divideButton.addEventListener("click", function() {
-    <input type="number" id="input1" placeholder="Enter a number">
-  <input type="number" id="input2" placeholder="Enter a number">
-
-  <button onclick="calculate('+')">Add</button>
-  <button onclick="calculate('-')">Subtract</button>
-  <button onclick="calculate('*')">Multiply</button>
-  <button onclick="calculate('/')">Divide</button>
-
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    let valueOfInput1 = input1.value;
+    let valueOfInput2 = input2.value;
+    let result = valueOfInput1/valueOfInput2;
 })
+console.log(result);
